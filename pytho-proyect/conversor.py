410723f0a373c8033893d2fecf9302e121c539d1
Junior Cruz <melonjr5@gmail.com>
2022-03-06 18:15:29 -0400

    actualizacion del programa python con varias versiones>

--- pytho-proyect/conversor.py
+++ pytho-proyect/conversor.py
@@ -1,6 +1,35 @@
-pesos= input("Cuantos pesoso tienes?: ")
-pesos = float(pesos)
-valor_dolar = 57.86
-resultado = pesos * valor_dolar
-print(resultado)
+#return - para regresar un resultado
+# def para crear funciones
+
+def conver(tipo_peso, dollar, opcion):
+    print("has seleccionado la " + opcion)
+    peso = input ("digite el monto a convertir de " + tipo_peso +" a dollar: ")
+    peso = float(peso)
+    dolar = (dollar)
+    resultado = peso / dolar
+    resultado = round(resultado, 3)
+    resultado = str(resultado)
+    print ("usted tiene:" + resultado + " dolares")
+menu = """
+bienvenidos al conversor de monedas, elija que tipo de moneda decea convertir
+ 
+ 1-pesos Dominicanos
+ 2-pesos Colombianos
+ 3- pesos Mexicano
+ 
+ elige una ocpion: """
+
+opcion = int(input(menu))
+if opcion == 1:
+    conver(" Pesos Dominicano", 57.64, "  opcion 1")
+
+elif opcion ==2:
+   conver(" Pesos Colombiano", 3875, "  opcion 2")
+elif opcion ==3:
+   conver(" Pesos Argentino", 65.57, "  opcion 3")
+else:
+    print("ingrece una opcopn valida que contenga solo numeros")
+
+
+
 
